aa4ec7c1d39f7947e9574b699a5ed57aad8061ff
boopathirajan <boopathirajan29102003@gmail.com>
2023-10-24 09:58:24 +0530

    '24/10/2023'

---
+++ useContext/Context.js
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from 'react'
+import React from 'react'
+import './context.css'
+
+const ThemeContext = createContext();
+
+function Context() {
+  const [theme,setTheme]=useState('light');
+  const handleTheme=()=>{
+    setTheme(theme==='light'?'dark':'light');
+  }
+  return (
+    <>
+      <p>select a theme</p>
+      <button onClick={handleTheme}>Toggle theme</button>
+    <p></p>
+    <ThemeContext.Provider value={theme}>
+      <Button ></Button>
+    </ThemeContext.Provider>
+    </>
+  )
+}
+
+const Button = (val) => {
+  const theme = useContext(ThemeContext);
+  console.log(theme)
+  return (
+    <panel title='welcome'>
+      <button className={theme} >Hello </button>
+    </panel>
+  )
+}
+
+export default Context
--- useLocation_useNavigation/Parent.js
+++ useLocation_useNavigation/Parent.js
@@ -1,24 +1,24 @@
-import { useLocation,useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import React from 'react'
 function Parent() {
-    const location=useLocation();
-    const navigate=useNavigate;
-    const info={message:"Succesfully data stored in parent"};
+    const location = useLocation();
+    const navigate = useNavigate;
+    const info = { message: "Succesfully data stored in parent" };
 
-    const data=location.state.data;
+    const data = location.state.data;
     console.log(data)
-    const handleClick=()=>{
-        navigate('child',{state:info});
+    const handleClick = () => {
+        navigate('child', { state: info });
     }
-  return (
-    <>
-    <p>Data from child</p>
-    <p>Name :{data.name}</p>
-    <p>Age :{data.age}</p>
-    <p onClick={handleClick}>Move to child</p>
-    </>
-  )
+    return (
+        <>
+            <p>Data from child</p>
+            <p>Name :{data.name}</p>
+            <p>Age :{data.age}</p>
+            <p onClick={handleClick}>Move to child</p>
+        </>
+    )
 }
 
 export default Parent
